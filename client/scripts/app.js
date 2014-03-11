1af60504fc490962b8bc6b738531dfaeee6b9ac2
Hack Reactor Students <rob@robertwholmes.com>
2014-03-11 16:13:38 -0700

    Refactored code to be contained in app object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,131 +1,148 @@
 /* Global _$ */
-var currentUserName = window.location.href.split('=')[1];
 
-var room = 'Default';
-var friendList = {};
-var roomList = {};
+var app = window.app;
 
-var sendMessage = function(message) {
-	var message = {
-    'username': currentUserName,
-    'text': message,
-    'roomname': room
-  };
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+app = {
 
-var getMessages = function() {
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    contentType: 'application/json',
-    data: 'order=-createdAt',
-    success: function(data) {
-      for (var i = 0; i < 100; i++) {
-        var username = data.results[i].username;
-        var message = data.results[i].text;
-        var chatRoom = data.results[i].roomname;
-        if (room === 'Default' || room === chatRoom) {
-	        if (message !== undefined && username && message && username.charAt(0) !== '<' && message.charAt(0) !== '<' && message.length < 160 && username.length < 160) {
-	        	if (friendList[username]) {
-							var $message = $('<li class="friend-message"></li>');
-	        	}
-	        	else {
-							var $message = $('<li></li>');
-	        	}
-						var $username = $('<a class="nameLink" href="#"></a>');
-						$username.text(username);
-						$message.text(': ' + message);
-						$message.prepend($username);
-						$('ul.messages').append($message);
-            if (chatRoom && !roomList[chatRoom]) {
-              roomList[chatRoom] = true;
-              renderRooms(chatRoom);
-            }
-	        }
-        }
-      }
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to get messages');
-    }
-	});
-	renderLinks();
-};
+  init: function() {
+    // variables
+    app.room = 'Default';
+    app.friendList = {};
+    app.roomList = {};
+    app.currentUserName = window.location.href.split('=')[1];
 
-var renderLinks = function() {
-	setTimeout(function(){
-		$('.nameLink').on('click', function(event) {
-			event.preventDefault();
-			if (!friendList[this.text]) {
-				friendList[this.text] = true;
-				$('ul.friends').append('<li><a class="currentFriend" href="#">' + this.text + '</a></li>');
-			}
-		});
-	}, 1200);
-};
+    // jquery selectors
+    app.$refresh = $('.refresh');
+    app.$ulMessages = $('ul.messages');
+    app.$send = $('.send');
+    app.$userMessage = $('.userMessage');
+    app.$sendRoomName = $('.sendRoomName');
+    app.$roomName = $('.roomName');
+    app.$roomsUl = $('ul.rooms');
+    app.$friendsUl = $('ul.friends');
 
-var renderRooms = function(currentRoom) {
-  var $room = $('<li class="room"></li>');
-  var $roomLink = $('<a class="roomLink" href="#"></a>').text(currentRoom);
-  $room.append($roomLink);
-  $('ul.rooms').append($room);
-};
-
-$(document).ready(function(){
-
-	getMessages();
-
-	$('.refresh').on('click', function(event) {
-		event.preventDefault();
-		$('ul.messages').empty();
-		getMessages();
-	});
-
-	$('.send').on('click', function(event) {
-		event.preventDefault();
-		var message = $('.userMessage').val();
-		sendMessage(message);
-		$('.userMessage').val('');
-		$('ul.messages').empty();
-		getMessages();
-	});
-
-	$('.sendRoomName').on('click', function(event) {
-		event.preventDefault();
-		room = $('.roomName').val() || 'Default';
-		$('.roomName').val('');
-		$('ul.messages').empty();
-		getMessages();
-	});
-
-  setTimeout(function() {
-    $('ul.rooms a').on('click', function(event) {
+    // jQuery listeners
+    app.$refresh.on('click', function(event) {
       event.preventDefault();
-      room = $(this).text() || 'Default';
-      console.log(room);
-      $('ul.messages').empty();
-      getMessages();
+      app.$ulMessages.empty();
+      app.getMessages();
     });
-  }, 1200);
 
-	renderLinks();
-});
+    app.$send.on('click', function(event) {
+      event.preventDefault();
+      var message = app.$userMessage.val();
+      app.sendMessage(message);
+      app.$userMessage.val('');
+      app.$ulMessages.empty();
+      app.getMessages();
+    });
+
+    app.$sendRoomName.on('click', function(event) {
+      event.preventDefault();
+      app.room = app.$roomName.val() || 'Default';
+      app.$roomName.val('');
+      app.$ulMessages.empty();
+      app.getMessages();
+    });
+
+    setTimeout(function() {
+      app.$ulRoomsA = $('ul.rooms a');
+      app.$ulRoomsA.on('click', function(event) {
+        event.preventDefault();
+        app.room = $(this).text() || 'Default';
+        app.$ulMessages.empty();
+        app.getMessages();
+      });
+    }, 1200);
+
+    app.getMessages();
+    app.renderLinks();
+
+  },
+
+  // methods
+  sendMessage: function(message) {
+    var message = {
+      'username': app.currentUserName,
+      'text': message,
+      'roomname': app.room
+    };
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+
+  getMessages: function() {
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      data: 'order=-createdAt',
+      success: function(data) {
+        for (var i = 0; i < 100; i++) {
+          var username = data.results[i].username;
+          var message = data.results[i].text;
+          var chatRoom = data.results[i].roomname;
+          if (app.room === 'Default' || app.room === chatRoom) {
+            if (message !== undefined && username && message && username.charAt(0) !== '<' && message.charAt(0) !== '<' && message.length < 160 && username.length < 160) {
+              if (app.friendList[username]) {
+                var $message = $('<li class="friend-message"></li>');
+              }
+              else {
+                var $message = $('<li></li>');
+              }
+              var $username = $('<a class="nameLink" href="#"></a>');
+              $username.text(username);
+              $message.text(': ' + message);
+              $message.prepend($username);
+              app.$ulMessages.append($message);
+              if (chatRoom && !app.roomList[chatRoom]) {
+                app.roomList[chatRoom] = true;
+                app.renderRooms(chatRoom);
+              }
+            }
+          }
+        }
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to get messages');
+      }
+    });
+    app.renderLinks();
+  },
+
+  renderLinks: function() {
+    setTimeout(function(){
+      $('.nameLink').on('click', function(event) {
+        event.preventDefault();
+        if (!app.friendList[this.text]) {
+          app.friendList[this.text] = true;
+          app.$friendsUl.append('<li><a class="currentFriend" href="#">' + this.text + '</a></li>');
+        }
+      });
+    }, 1200);
+  },
+
+  renderRooms: function(currentRoom) {
+    var $room = $('<li class="room"></li>');
+    var $roomLink = $('<a class="roomLink" href="#"></a>').text(currentRoom);
+    $room.append($roomLink);
+    app.$roomsUl.append($room);
+  }
+
+};
 
 
 
